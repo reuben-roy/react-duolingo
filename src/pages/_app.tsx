2856d09e28bf3289698278e69075d49f4dd5abaf
reuben-roy <reubenroyk@gmail.com>
2024-11-17 17:22:20 -0700

    feat: initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,10 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>React Duolingo Clone</title>
+        <title>Bars Scheduler</title>
         <meta
           name="description"
-          content="Duolingo web app clone written with React"
+          content="Auto scheduler written with React"
         />
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#0A0" />
